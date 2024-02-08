f33905a2f8bbcfbaad27bb92ad8a747f1602742a
Matt <main@mchmatt.com>
2024-02-08 11:30:30 -0300

    Use Windows sandboxing

--- src/main_win.cc
+++ src/main_win.cc
@@ -14,10 +14,19 @@ int APIENTRY wWinMain(
 ) {
     printf("Starting helper process\n");
 
+    void* sandbox_info = nullptr;
+
+    #if defined(CEF_USE_SANDBOX)
+    // Manage the life span of the sandbox information object. This is necessary
+    // for sandbox support on Windows. See cef_sandbox_win.h for complete details.
+    CefScopedSandboxInfo scoped_sandbox;
+    sandbox_info = scoped_sandbox.sandbox_info();
+    #endif
+
     CefRefPtr<OverlayRenderProcessHandler> renderProcessHandler = new OverlayRenderProcessHandler();
     CefRefPtr<OverlayApp> app = new OverlayApp(renderProcessHandler);
 
     CefMainArgs mainArgs(hInstance);
-    return CefExecuteProcess(mainArgs, app, nullptr);
+    return CefExecuteProcess(mainArgs, app, sandbox_info);
 }
 #endif
